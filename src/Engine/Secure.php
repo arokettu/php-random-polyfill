f306385103cd6f6837dfdb77202dbbbd2268086a
Anton Smirnov <sandfox@sandfox.me>
2022-07-22 05:12:02 +0300

    Throw on serialization

--- src/Engine/Secure.php
+++ src/Engine/Secure.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 namespace Random\Engine;
 
+use Exception;
 use Random\CryptoSafeEngine;
+
+use function random_bytes;
 
 final class Secure implements CryptoSafeEngine
 {
     public function generate(): string
     {
         return random_bytes(PHP_INT_SIZE);
+    }
+
+    public function __sleep(): array
+    {
+        throw new Exception("Serialization of 'Random\Engine\Secure' is not allowed");
     }
 }
