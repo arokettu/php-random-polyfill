0e3213d4c9f075c5c815ca8b1bebb961cf66deb7
Anton Smirnov <sandfox@sandfox.me>
2022-07-23 06:00:50 +0300

    Point to the upstream sources

--- src/Engine/Mt19937.php
+++ src/Engine/Mt19937.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * @copyright Copyright © 2022 Anton Smirnov
+ * @license BSD-3-Clause https://spdx.org/licenses/BSD-3-Clause.html
+ *
+ * Includes adaptation of C code from the PHP Interpreter
+ * @license PHP-3.01 https://spdx.org/licenses/PHP-3.01.html
+ * @see https://github.com/php/php-src/blob/master/ext/random/engine_mt19937.c
+ */
+
 declare(strict_types=1);
 
 namespace Random\Engine;
