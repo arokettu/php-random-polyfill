a7a45ff3c0dbbc28823afb4712900f9a7df5f168
Anton Smirnov <sandfox@sandfox.me>
2022-08-17 17:10:36 +0300

    Remove todo

--- debug/shuffle.php
+++ debug/shuffle.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 //$rnd = new \Random\Randomizer(new \Random\Engine\Secure());
-//$rnd = new \Random\Randomizer(new \Arokettu\Random\Tests\DevEngines\Xorshift32(123)); // TODO: different results
+//$rnd = new \Random\Randomizer(new \Arokettu\Random\Tests\DevEngines\Xorshift32(123));
 //$rnd = new \Random\Randomizer(new \Random\Engine\Mt19937(123, MT_RAND_MT19937));
 $rnd = new \Random\Randomizer(new \Random\Engine\Mt19937(123, MT_RAND_PHP));
 //$rnd = new \Random\Randomizer(new \Arokettu\Random\Tests\DevEngines\Zeros());
