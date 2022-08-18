c4f6cfaf00aff3bde166f9b3195c03c39338c52e
Anton Smirnov <sandfox@sandfox.me>
2022-08-18 22:46:18 +0300

    Stubs

---
+++ tests/FromPHP/Randomizer/PickArrayKeysTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arokettu\Random\Tests\FromPHP\Randomizer;
+
+/**
+ * @see https://github.com/php/php-src/blob/master/ext/random/tests/03_randomizer/pick_array_keys.phpt
+ */
+class PickArrayKeysTest
+{
+    // skip, known incompatibility
+}
