947b9a680721c6b6a9a98663038bce4c2ab3f527
Anton Smirnov <sandfox@sandfox.me>
2022-08-19 22:23:30 +0300

    user_unsafe.phpt: Empty string

--- tests/DevEngines/EmptyStringEngine.php
+++ tests/DevEngines/EmptyStringEngine.php
@@ -6,7 +6,10 @@ namespace Arokettu\Random\Tests\DevEngines;
 
 use Random\Engine;
 
-final class Fail implements Engine
+/**
+ * @see https://github.com/php/php-src/blob/master/ext/random/tests/03_randomizer/user_unsafe.phpt
+ */
+final class EmptyStringEngine implements Engine
 {
     public function generate(): string
     {
---
+++ tests/FromPHP/Randomizer/UserUnsafeTest.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arokettu\Random\Tests\FromPHP\Randomizer;
+
+use Arokettu\Random\Tests\DevEngines\EmptyStringEngine;
+use PHPUnit\Framework\TestCase;
+use Random\BrokenRandomEngineError;
+use Random\Randomizer;
+use RuntimeException;
+use Throwable;
+
+/**
+ * @see https://github.com/php/php-src/blob/master/ext/random/tests/03_randomizer/user_unsafe.phpt
+ */
+class UserUnsafeTest extends TestCase
+{
+    public function testEmptyStringEngineGetInt(): void
+    {
+        try {
+            (new Randomizer(new EmptyStringEngine()))->getInt(0, 123);
+        } catch (Throwable $e) {
+            self::assertEquals(BrokenRandomEngineError::class, get_class($e));
+            self::assertEquals(
+                'A random engine must return a non-empty string',
+                $e->getMessage()
+            );
+            self::assertEquals(0, $e->getCode());
+            self::assertNull($e->getPrevious());
+
+            return;
+        }
+
+        throw new RuntimeException('Throwable expected'); // do not use expectException to test getPrevious()
+    }
+
+    public function testEmptyStringEngineNextInt(): void
+    {
+        try {
+            (new Randomizer(new EmptyStringEngine()))->nextInt();
+        } catch (Throwable $e) {
+            self::assertEquals(BrokenRandomEngineError::class, get_class($e));
+            self::assertEquals(
+                'A random engine must return a non-empty string',
+                $e->getMessage()
+            );
+            self::assertEquals(0, $e->getCode());
+            self::assertNull($e->getPrevious());
+
+            return;
+        }
+
+        throw new RuntimeException('Throwable expected'); // do not use expectException to test getPrevious()
+    }
+
+    public function testEmptyStringEngineGetBytes(): void
+    {
+        try {
+            (new Randomizer(new EmptyStringEngine()))->getBytes(1);
+        } catch (Throwable $e) {
+            self::assertEquals(BrokenRandomEngineError::class, get_class($e));
+            self::assertEquals(
+                'A random engine must return a non-empty string',
+                $e->getMessage()
+            );
+            self::assertEquals(0, $e->getCode());
+            self::assertNull($e->getPrevious());
+
+            return;
+        }
+
+        throw new RuntimeException('Throwable expected'); // do not use expectException to test getPrevious()
+    }
+
+    public function testEmptyStringEngineShuffleArray(): void
+    {
+        try {
+            (new Randomizer(new EmptyStringEngine()))->shuffleArray(\range(1, 10));
+        } catch (Throwable $e) {
+            self::assertEquals(BrokenRandomEngineError::class, get_class($e));
+            self::assertEquals(
+                'A random engine must return a non-empty string',
+                $e->getMessage()
+            );
+            self::assertEquals(0, $e->getCode());
+            self::assertNull($e->getPrevious());
+
+            return;
+        }
+
+        throw new RuntimeException('Throwable expected'); // do not use expectException to test getPrevious()
+    }
+
+    public function testEmptyStringEngineShuffleBytes(): void
+    {
+        try {
+            (new Randomizer(new EmptyStringEngine()))->shuffleBytes('foobar');
+        } catch (Throwable $e) {
+            self::assertEquals(BrokenRandomEngineError::class, get_class($e));
+            self::assertEquals(
+                'A random engine must return a non-empty string',
+                $e->getMessage()
+            );
+            self::assertEquals(0, $e->getCode());
+            self::assertNull($e->getPrevious());
+
+            return;
+        }
+
+        throw new RuntimeException('Throwable expected'); // do not use expectException to test getPrevious()
+    }
+}
