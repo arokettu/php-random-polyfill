438612e0905654cbbfff58224b8665f98e9badb2
Anton Smirnov <sandfox@sandfox.me>
2022-08-19 22:45:00 +0300

    user_exits.phpt

---
+++ tests/FromPHP/Randomizer/UserExitsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arokettu\Random\Tests\FromPHP\Randomizer;
+
+/**
+ * @see https://github.com/php/php-src/blob/master/ext/random/tests/03_randomizer/user_exits.phpt
+ */
+class UserExitsTest
+{
+    // Let's trust the engine here
+}
